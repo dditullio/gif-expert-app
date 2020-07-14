957269c8412c8b31bf1a66cb5e31ed8ddd31f627
Daniel <danieldt2000@hotmail.com>
2020-07-14 12:50:34 -0300

    Actualización del nombre de la carpeta de distribución

---
+++ docs/precache-manifest.d9eae9db5ae368124708449cbce9e7f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bdf1549db80c1cf60bf4781c6ac562f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f0df16b81c4a67c9861",
+    "url": "/static/css/main.c638d095.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0df16b81c4a67c9861",
+    "url": "/static/js/main.503963c7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
