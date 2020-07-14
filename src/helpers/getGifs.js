f5d6ea5b1ec1127f6efc17c370c6dd7f916a384b
Daniel <danieldt2000@hotmail.com>
2020-07-14 12:10:36 -0300

    Primera versión finalizada

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=KQHB5q1T5gjzBFl8P7QrQwUU8ZRK4I4U`;
+  const resp = await fetch(url);
+
+  const { data } = await resp.json();
+
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+  return gifs;
+};
